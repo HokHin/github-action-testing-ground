565df05e17d9b2421e99121e969125c62b873297
HokHin Leung <74968437+HokHin@users.noreply.github.com>
2024-04-03 10:31:45 +0100

    Added console writeline

--- GitVersionTest/GitVersionTest/Program.cs
+++ GitVersionTest/GitVersionTest/Program.cs
@@ -14,3 +14,5 @@ Console.WriteLine("Hello, World! Another topic branch");
 Console.WriteLine("Hello, World! Adding feature branch");
 
 Console.WriteLine("Hello, World! Adding topic branch");
+
+Console.WriteLine("Hello, World! Adding another topic branch");
